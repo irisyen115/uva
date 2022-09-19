b43e415cff8b25e96ba2bb297bd19b5dfdd03a41
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-09-19 19:56:29 +0800

    w38 p1 2.5/14 extra points:1.5 Ecological Bin Packing

--- 102.py
+++ 102.py
@@ -2,24 +2,18 @@ from sys import stdin
 
 def process(line):    
     bottoms = list(map(int, line.split(' ')))
-    First = [bottoms[i] for i in range(3)]
-    Second = [bottoms[i] for i in range(3,6)]
-    Third = [bottoms[i] for i in range(6,9)]        
+    first, second, third = bottoms[:3], bottoms[3:6], bottoms[6:]       
     names = ["BGC","BCG","GBC","GCB","CBG","CGB"]
     count = [0] * 6
-    count[0] = (First[1]+First[2])+(Second[0]+Second[2])+(Third[0]+Third[1])
-    count[1] = (First[1]+First[2])+(Second[0]+Second[1])+(Third[0]+Third[2])
-    count[2] = (First[0]+First[2])+(Second[1]+Second[2])+(Third[0]+Third[1])
-    count[3] = (First[0]+First[2])+(Second[0]+Second[1])+(Third[1]+Third[2])
-    count[4] = (First[0]+First[1])+(Second[1]+Second[2])+(Third[0]+Third[2])
-    count[5] = (First[0]+First[1])+(Second[0]+Second[2])+(Third[1]+Third[2])
-    c = sorted(zip(count, names))
-    res, name = c[0]
-    print(name + " " + str(res))
-    
-    
-
-
+    count[0] = (first[1]+first[2])+(second[0]+second[2])+(third[0]+third[1])
+    count[1] = (first[1]+first[2])+(second[0]+second[1])+(third[0]+third[2])
+    count[2] = (first[0]+first[2])+(second[1]+second[2])+(third[0]+third[1])
+    count[3] = (first[0]+first[2])+(second[0]+second[1])+(third[1]+third[2])
+    count[4] = (first[0]+first[1])+(second[1]+second[2])+(third[0]+third[2])
+    count[5] = (first[0]+first[1])+(second[0]+second[2])+(third[1]+third[2])
+    res, name = min(zip(count, names))
+    print(min(zip(count, names)))
+    print(f'{name} {res}')
 
 for line in stdin:
     if line.strip() == "": # If empty string is read then stop the loop
